a91611fd28b43358a70be2e211d0c8643036eaf9
Joshua Toguri <joshua.toguri@gmail.com>
2022-06-13 17:43:12 -0400

    refactor Projects component to be indepenant of the projects themselves

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,37 +3,24 @@ import './Projects.css';
 import projects from '../data/projects.json';
 
 export default function Projects() {
+  const projectHTML = projects.map(el => {
+    return (
+      <div className="project">
+        <img src="#" alt="Project screen-shot"/>
+        <h3>{el.name}</h3>
+        <p>{el.description}</p>
+        <ul>
+          <li><a href="#">active link</a></li>
+          <li><a href="#">view src code</a></li>
+        </ul>
+      </div>
+    )
+  });
   return (
     <div id="Projects">
       <h2>Projects</h2>
       <div className="project-container">
-        <div className="project">
-          <img src="#" alt="Project screen-shot"/>
-          <h3>MVMT Break</h3>
-          <p>Description</p>
-          <ul>
-            <li><a href="#">active link</a></li>
-            <li><a href="#">view src code</a></li>
-          </ul>
-        </div>
-        <div className="project">
-          <img src="#" alt="Project screen-shot"/>
-          <h3>Decision Wizard</h3>
-          <p>Description</p>
-          <ul>
-            <li><a href="#">active link</a></li>
-            <li><a href="#">view src code</a></li>
-          </ul>
-        </div>
-        <div className="project">
-          <img src="#" alt="Project screen-shot"/>
-          <h3>Another Project</h3>
-          <p>Description</p>
-          <ul>
-            <li><a href="#">active link</a></li>
-            <li><a href="#">view src code</a></li>
-          </ul>
-        </div>
+        {projectHTML}
       </div>
     </div>
   )
