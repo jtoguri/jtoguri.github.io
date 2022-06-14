520455a9a717ef72fe143c8be616c39137507acf
Joshua Toguri <joshua.toguri@gmail.com>
2022-06-14 15:03:54 -0400

    use json file to store skills data

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,38 +1,24 @@
 import './Skills.css';
+import skills from '../data/skills.json';
 
 export default function Skills() {
+
+  const skillsContainer = skills.map(el => {
+    return (
+      <div className="skill-category">
+        <h4>{el.category}</h4>
+        <ul>
+          {el.list.map(skill => <li>{skill}</li>)}
+        </ul>
+      </div>
+    );
+  });
+
   return (
     <div id="Skills">
       <h2>Skills</h2>
       <div className="skills-container">
-        <div className="skill">
-          <h4>Languages</h4>
-          <ul>
-            <li>JavaScript</li>
-            <li>Bash</li>
-            <li>SQL</li>
-            <li>HTML</li>
-            <li>CSS</li>
-          </ul>
-        </div>
-        <div className="skill">
-          <h4>Frameworks</h4>
-          <ul>
-            <li>PostgreSQL</li>
-            <li>MongoDB</li>
-            <li>React</li>
-            <li>JQuery</li>
-            <li>Bootstrap</li>
-          </ul>
-        </div>
-        <div className="skill">
-          <h4>Potential others to include ?</h4>
-          <ul>
-            <li>Vim</li>
-            <li>Linux</li>
-            <li>Docker</li>
-          </ul>
-        </div>
+        {skillsContainer}
       </div>
     </div>
   )
