afb612891ed069c6e45c32570aeb19967296e63c
Joshua Toguri <joshua.toguri@gmail.com>
2022-08-26 19:36:51 -0400

    test getting rid of full vh coverage for the home cover

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
 
 import Content from './components/Content';
@@ -6,6 +8,15 @@ import Header from './components/Header';
 import HomeCover from './components/HomeCover';
 
 function App() {
+  const [vh, setVh] = useState({});
+
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    setVh({
+      "--vh": `${vh}px`
+    });
+  }, []);
+
   return (
     <div className="App"> 
       <Header />
