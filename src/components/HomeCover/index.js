73812248a4e0efa2692778658604eed9517616b8
Joshua Toguri <joshua.toguri@gmail.com>
2022-08-27 12:47:29 -0400

    update home cover

--- src/components/HomeCover/index.js
+++ src/components/HomeCover/index.js
@@ -7,17 +7,18 @@ import './HomeCover.scss';
 export default function HomeCover () {
   return (
     <div className='home-cover'>
-      <img className="headshot-img" src={headshot} />
-      <div className='about'>
-        <h1>Hi 👋 I'm Josh.</h1>
-        <p>
+      <img className='headshot-img' src={headshot} />
+      <div className='cover-content'>
+        <h1 className='cover-greeting'>Hi 👋 I'm Josh.</h1>
+        {/*<p>
           I'm a software engineer with experience in
           engineering sales and a degree in Biological 
           Engineering. After deciding to transition into software
           development, I completed a full-stack web development bootcamp.
           Currently, I am working as the sole developer at a small
           manufacturing start-up.
-        </p>
+        </p>*/}
+        <span className='cover-bio'>Software Engineer, Athlete, Lifelong Learner</span>
         <Socials />
       </div>
     </div>
